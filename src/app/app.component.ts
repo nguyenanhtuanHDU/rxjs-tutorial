1663794ef0cf154c8f35902cae9c8087eeb97b6e
nguyenanhtuanHDU <anhtuan02042002@gmail.com>
2023-07-13 09:35:10 +0700

    observable

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'rxjs-tutorial';
+
+  listUsers!: Observable<string>;
+  user!: string;
+
+  ngOnInit(): void {
+    this.listUsers = new Observable((subscriber) => {
+      subscriber.next('user 1');
+      subscriber.next('user 2');
+      subscriber.next('user 3');
+    });
+    this.listUsers.subscribe((data: string) => {
+      console.log(`🚀 ~ data:`, data);
+      this.user = data;
+      // user 1
+      // user 2
+      // user 3
+    });
+  }
 }
