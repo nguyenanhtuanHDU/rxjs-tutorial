0eb73f827897decbfb374b57187869e48a4a6afc
nguyenanhtuanHDU <anhtuan02042002@gmail.com>
2023-07-18 16:28:11 +0700

    merge multiple Observables

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -18,6 +18,7 @@ import {
   last,
   map,
   max,
+  merge,
   of,
   skip,
   take,
@@ -33,16 +34,14 @@ import { AppService } from './app.service';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  arr1: Observable<number> = from([1, 2]);
-  arr2: Observable<number> = from([3, 4]);
-  arr3: Observable<number> = from([5, 6]);
+  arr1: Observable<number> = from([1, 1]);
+  arr2: Observable<number> = from([2, 2]);
+  arr3: Observable<number> = from([3, 3]);
 
   ngOnInit(): void {
-    concat(this.arr1, this.arr2, this.arr3).subscribe((data) => {
-      console.log(data);
+    merge(this.arr1, this.arr2, this.arr3).subscribe((data) => {
+      console.log(data); // in ra 1,1, 2,2,3,3 cùng lúc
     });
-
-    // concat(Observable 1,2,...): các Observable sẽ chạy lần lượt
-    // Observable thứ 2 chỉ chạy khi Observable 1 complete
+    // merge(Observable 1,2,3...): các Observable chạy cùng lúc
   }
 }
